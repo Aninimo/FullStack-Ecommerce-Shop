f234ec0f9c8aa0d041233b3d2d251a887364c82c
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-10-04 13:37:59 -0300

    Update get-categories.tsx

--- src/actions/get-categories.tsx
+++ src/actions/get-categories.tsx
@@ -1,11 +1,12 @@
-import { Category } from '../types'
+import { Category } from '../types';
 
-const URL='https://full-stack-ecommerce-dashboard.vercel.app/api/650ccc0ab7bfd9b0b3b62959/categories'
+const URL = 'https://full-stack-ecommerce-dashboard.vercel.app/api/650ccc0ab7bfd9b0b3b62959/categories';
 
 const getCategories = async (): Promise<Category[]> => {
-  const res = await fetch(URL)
+  const res = await fetch(URL);
+  const data = await res.json(); 
 
-  return res.json()
-}
+  return data;
+};
 
-export default getCategories
+export default getCategories;
